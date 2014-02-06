20a94cf2fdcfa0c52ea6a06e1aa57c76ad8df128
AnujDhanju <anuj.kumar.x@venturepact.com>
2014-02-07 00:11:39 +0530

    'validation'

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  test "product is not valid without a unique title" do
+	product = Product.new(:title => products(:one).title,
+	:description => "yyy" ,
+	:price => 1,
+	:image_url => "fred.gif" )
+	assert !product.save
+	assert_equal "has already been taken" , product.errors[:title].join('; ' )
+	
+	end
+	
+	
 end
